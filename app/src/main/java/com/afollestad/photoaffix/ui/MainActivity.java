31d1f05f14b29c629f18490ca679be699521ce54
plusCubed <plusCubed@users.noreply.github.com>
2015-10-27 18:22:17 -0700

    Use inSampleSize (for now)

--- app/src/main/java/com/afollestad/photoaffix/ui/MainActivity.java
+++ app/src/main/java/com/afollestad/photoaffix/ui/MainActivity.java
@@ -382,32 +382,8 @@ public class MainActivity extends AppCompatActivity implements
         return bm;
     }
 
-    public static void calculateInSampleSize(
-            BitmapFactory.Options options, int reqWidth, int reqHeight) {
-        // Raw height and width of image
-        final int height = options.outHeight;
-        final int width = options.outWidth;
-        int inSampleSize = 1;
-
-        if (height > reqHeight || width > reqWidth) {
-
-            final int halfHeight = height / 2;
-            final int halfWidth = width / 2;
-
-            // Calculate the largest inSampleSize value that is a power of 2 and keeps both
-            // height and width larger than the requested height and width.
-            while ((halfHeight / inSampleSize) > reqHeight
-                    && (halfWidth / inSampleSize) > reqWidth) {
-                inSampleSize *= 2;
-            }
-        }
-
-        //OPTION 1 - inSampleSize
-        // options.inSampleSize = inSampleSize;
-
-        //OPTION 2 - inDensity
-        options.inDensity = height / reqHeight;
-        options.inTargetDensity = 1;
+    public static void calculateInSampleSize(BitmapFactory.Options options, int reqHeight) {
+        options.inSampleSize = options.outHeight / reqHeight;
     }
 
     private void startProcessing() {
@@ -575,7 +551,7 @@ public class MainActivity extends AppCompatActivity implements
                                 dstRect.left, dstRect.right, dstRect.top, dstRect.bottom);
 
                         bitmapOptions.inJustDecodeBounds = false;
-                        calculateInSampleSize(bitmapOptions, (dstRect.right - dstRect.left), dstRect.bottom - dstRect.top);
+                        calculateInSampleSize(bitmapOptions, dstRect.bottom - dstRect.top);
 
                         Bitmap bm = getNextBitmap(bitmapOptions);
                         if (bm == null) {
